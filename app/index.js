2bbbb3e4078b9f324c17dd095ed05f54ff207ac6
Kevin Bai <kevin.bai.sin@gmail.com>
2019-01-28 23:51:06 +0800

    临时

--- app/index.js
+++ app/index.js
@@ -1,28 +1,53 @@
+// import React from 'react';
+// import { render } from 'react-dom';
+// import { AppContainer } from 'react-hot-loader';
+// import Root from './views/index';
+// // import { configureStore, history } from './store/configureStore';
+// import './app.global.css';
+//
+// // const store = configureStore();
+//
+// render(
+//   <AppContainer>
+//     <Root />
+//   </AppContainer>,
+//   document.getElementById('root')
+// );
+//
+// if (module.hot) {
+//   module.hot.accept('./views/index', () => {
+//     // eslint-disable-next-line global-require
+//     const NextRoot = require('./views/index').default;
+//     render(
+//       <AppContainer>
+//         <NextRoot />
+//       </AppContainer>,
+//       document.getElementById('root')
+//     );
+//   });
+// }
+//
+
+/* @flow */
 import React from 'react';
 import { render } from 'react-dom';
-import { AppContainer } from 'react-hot-loader';
-import Root from './containers/Root';
-import { configureStore, history } from './store/configureStore';
-import './app.global.css';
+import { Normalize } from 'styled-normalize';
+import BaseStyles from 'support/styles';
+import App from 'views/index';
 
-const store = configureStore();
-
-render(
-  <AppContainer>
-    <Root store={store} history={history} />
-  </AppContainer>,
-  document.getElementById('root')
-);
-
-if (module.hot) {
-  module.hot.accept('./containers/Root', () => {
-    // eslint-disable-next-line global-require
-    const NextRoot = require('./containers/Root').default;
-    render(
-      <AppContainer>
-        <NextRoot store={store} history={history} />
-      </AppContainer>,
-      document.getElementById('root')
-    );
-  });
+const root: ?HTMLElement = document.getElementById('root');
+if (root) {
+  render(
+    <React.Fragment>
+      <Normalize />
+      <BaseStyles />
+      <App />
+    </React.Fragment>,
+    root,
+  );
 }
+
+window.onbeforeunload = () => {
+  // $FlowIssue: render empty component
+  render(null, root);
+};
