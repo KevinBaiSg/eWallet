c4d78e53b15e2f8a902167fdf6b5d5a893b01759
Kevin Bai <kevin.bai.sin@gmail.com>
2019-02-18 00:28:20 +0800

    整理文件

--- app/views/Landing/views/Root/index.js
+++ app/views/Landing/views/Root/index.js
@@ -6,19 +6,7 @@ import {
   observer,
 } from 'mobx-react'
 import PropTypes from 'prop-types'
-import { canUseDOM } from 'exenv'
 import { AppState } from 'store';
-import { Redirect } from 'react-router-dom'
-// import { stringToHex } from 'utils/bufferUtils'
-// import * as bitcoin from "bitcoinjs-lib-zcash";
-// import {
-//   getCoinInfoByCurrency,
-//   parseCoinsJson,
-// } from 'utils/data/CoinInfo';
-// import { CoinsJson } from 'utils/data/coins'
-// import {create as createBackend} from 'utils/backend'
-
-// import { DeviceList } from 'trezor.js';
 import LandingWrapper from 'views/Landing/components/LandingWrapper'
 
 // import BetaDisclaimer from 'views/Landing/components/BetaDisclaimer';
@@ -41,17 +29,13 @@ class Root extends Component<Props> {
   }
 
   render() {
-    if (!canUseDOM) { return null }
-
     const { appState } = this.props;
 
-    if (appState.deviceConnected === true) {
-      console.log('appState.deviceConnected === true');
-      this.props.history.replace('/bridge');
-      return null
-    }
+    // if (appState.deviceConnected === true) {
+    //   this.props.history.replace('/bridge');
+    //   return null
+    // }
 
-    // window.ipcRenderer.send('synchronous-message', 'ping');
     return (
       <LandingWrapper loading />
     );
@@ -68,26 +52,6 @@ export default (inject((stores) => {
   }
 })(observer(Root)))
 
-
-// list.on('connectUnacquired', u => {
-//   console.log(`DeviceList connectUnacquired; UnacquiredDevice: ${u}`);
-// });
-//
-// list.on('transport', t => {
-//   console.log(`DeviceList  transport is successfully initialized; Transport: ${t}`);
-// });
-//
-// list.on('disconnect', d => {
-//   console.log(`DeviceList device is disconnected; Device: ${d}`);
-// });
-//
-// list.on('disconnectUnacquired', u => {
-//   console.log(`DeviceList unacquired device is disconnected; UnacquiredDevice: ${u}`);
-// });
-//
-// list.on('error', e => {
-//   console.log(`DeviceList initialization error: ${e}`);
-// });
 
 
 
