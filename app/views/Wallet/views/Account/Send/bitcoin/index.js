b01d4ba41fdf070419d66ff4858a33ee0a7d13f1
Kevin Bai <kevin.bai.sin@gmail.com>
2019-03-09 23:53:49 +0800

    增加 address 输入框

--- app/views/Wallet/views/Account/Send/bitcoin/index.js
+++ app/views/Wallet/views/Account/Send/bitcoin/index.js
@@ -1,19 +1,289 @@
 /* @flow */
 
 import React from 'react';
-// import { Select } from 'components/Select';
+import PropTypes from 'prop-types';
+import BigNumber from 'bignumber.js';
+import styled, { css } from 'styled-components';
+import { Select } from 'components/Select';
+import Button from 'components/Button';
+import Input from 'components/inputs/Input';
+import Icon from 'components/Icon';
+import Link from 'components/Link';
+import ICONS from 'config/icons';
 import { FONT_SIZE, FONT_WEIGHT, TRANSITION } from 'config/variables';
+import colors from 'config/colors';
 import Title from 'views/Wallet/components/Title';
+import P from 'components/Paragraph';
 import Content from 'views/Wallet/components/Content';
-import { H1 } from 'components/Heading';
+// import * as stateUtils from 'reducers/utils';
+import type { Token } from 'flowtype';
+// import AdvancedForm from './components/AdvancedForm';
+// import PendingTransactions from '../components/PendingTransactions';
+import { inject, observer } from 'mobx-react';
 
-// stateless component
-const AccountSend = (props: Props) => {
-  return (
-    <Content>
-      <Title>Send Bitcoin(BTC)</Title>
-    </Content>
-  );
+// TODO: Decide on a small screen width for the whole app
+// and put it inside config/variables.js
+const SmallScreenWidth = '850px';
+
+const AmountInputLabelWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const AmountInputLabel = styled.span`
+    text-align: right;
+    color: ${colors.TEXT_SECONDARY};
+`;
+
+const InputRow = styled.div`
+    padding-bottom: 28px;
+`;
+
+const SetMaxAmountButton = styled(Button)`
+    height: 40px;
+    padding: 0 10px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: ${FONT_SIZE.SMALL};
+    font-weight: ${FONT_WEIGHT.SMALLEST};
+    color: ${colors.TEXT_SECONDARY};
+
+    border-radius: 0;
+    border: 1px solid ${colors.DIVIDER};
+    border-right: 0;
+    border-left: 0;
+    background: transparent;
+    transition: ${TRANSITION.HOVER};
+
+    &:hover {
+        background: ${colors.GRAY_LIGHT};
+    }
+
+    ${props => props.isActive && css`
+        color: ${colors.WHITE};
+        background: ${colors.GREEN_PRIMARY};
+        border-color: ${colors.GREEN_PRIMARY};
+
+        &:hover {
+            background: ${colors.GREEN_SECONDARY};
+        }
+
+        &:active {
+            background: ${colors.GREEN_TERTIARY};
+        }
+    `}
+`;
+
+const CurrencySelect = styled(Select)`
+    min-width: 77px;
+    height: 40px;
+    flex: 0.2;
+`;
+
+const FeeOptionWrapper = styled.div`
+    display: flex;
+    justify-content: space-between;
+`;
+
+const OptionValue = styled(P)`
+    flex: 1 0 auto;
+    min-width: 70px;
+    margin-right: 5px;
+`;
+
+const OptionLabel = styled(P)`
+    flex: 0 1 auto;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    text-align: right;
+    word-break: break-all;
+`;
+
+const FeeLabelWrapper = styled.div`
+    display: flex;
+    align-items: center;
+    padding-bottom: 10px;
+`;
+
+const FeeLabel = styled.span`
+    color: ${colors.TEXT_SECONDARY};
+`;
+
+const UpdateFeeWrapper = styled.span`
+    margin-left: 8px;
+    display: flex;
+    align-items: center;
+    font-size: ${FONT_SIZE.SMALL};
+    color: ${colors.WARNING_PRIMARY};
+`;
+
+const StyledLink = styled(Link)`
+    margin-left: 4px;
+    white-space: nowrap;
+`;
+
+const ToggleAdvancedSettingsWrapper = styled.div`
+    min-height: 40px;
+    margin-bottom: 20px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+
+    @media screen and (max-width: ${SmallScreenWidth}) {
+        ${props => (props.isAdvancedSettingsHidden && css`
+            flex-direction: column;
+        `)}
+    }
+`;
+
+const ToggleAdvancedSettingsButton = styled(Button)`
+    min-height: 40px;
+    padding: 0;
+    display: flex;
+    flex: 1 1 0;
+    align-items: center;
+    font-weight: ${FONT_WEIGHT.SEMIBOLD};
+`;
+
+const FormButtons = styled.div`
+    display: flex;
+    flex: 1 1;
+
+    
+    @media screen and (max-width: ${SmallScreenWidth}) {
+        margin-top: ${props => (props.isAdvancedSettingsHidden ? '10px' : 0)};
+    }
+
+    Button + Button {
+        margin-left: 5px;
+    }
+`;
+
+const SendButton = styled(Button)`
+    word-break: break-all;
+    flex: 1;
+
+`;
+
+const ClearButton = styled(Button)`
+
+`;
+
+const AdvancedSettingsIcon = styled(Icon)`
+    margin-left: 10px;
+`;
+
+const QrButton = styled(Button)`
+    border-top-left-radius: 0px;
+    border-bottom-left-radius: 0px;
+    border-left: 0px;
+    height: 40px;
+    padding: 0 10px;
+`;
+
+class AccountSend extends React.Component<Props> {
+  state = {
+    // for address
+    address: '',
+    addressErrors: null,
+    addressWarnings: null,
+    addressInfos: null,
+    // for amount
+    amountErrors: null,
+    amountWarnings: null,
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      // for address
+      address: '',
+      addressErrors: null,
+      addressWarnings: null,
+      addressInfos: null,
+      // for amount
+      amountErrors: null,
+      amountWarnings: null,
+    };
+
+    // this.intervalId = false
+    this.getAddressInputState = this.getAddressInputState.bind(this);
+    this.getAmountInputState = this.getAmountInputState.bind(this);
+    this.onAddressChange = this.onAddressChange.bind(this);
+  }
+
+  getAddressInputState(): string {
+    let state = '';
+    if (this.state.address && !this.state.addressErrors) {
+      state = 'success';
+    }
+    if (this.state.addressWarnings && !this.state.addressErrors) {
+      state = 'warning';
+    }
+    if (this.state.addressErrors) {
+      state = 'error';
+    }
+    return state;
+  };
+
+  getAmountInputState(): string {
+    let state = '';
+    if (this.state.amountWarnings && !this.state.amountErrors) {
+      state = 'warning';
+    }
+    if (this.state.amountErrors) {
+      state = 'error';
+    }
+    return state;
+  };
+
+  onAddressChange(address: string) {
+    this.setState({ address: address });
+  }
+
+  render() {
+    return (
+      <Content>
+        <Title>Send Bitcoin(BTC)</Title>
+        <InputRow>
+          <Input
+            state={this.getAddressInputState()}
+            autoComplete="off"
+            autoCorrect="off"
+            autoCapitalize="off"
+            spellCheck="false"
+            topLabel="Address"
+            bottomText={this.state.addressErrors || this.state.addressWarnings || this.state.addressInfos}
+            value={this.state.address}
+            onChange={event => this.onAddressChange(event.target.value)}
+            sideAddons={[(
+              <QrButton
+                key="qrButton"
+                isWhite
+                // onClick={props.openQrModal}
+              >
+                <Icon
+                  size={25}
+                  color={colors.TEXT_SECONDARY}
+                  icon={ICONS.QRCODE}
+                />
+              </QrButton>
+            )]}
+          />
+        </InputRow>
+      </Content>
+    );
+  }
+}
+
+AccountSend.propTypes = {
+  appState: PropTypes.object.isRequired
 };
 
-export default AccountSend;
+export default inject((stores) => {
+  return {
+    appState: stores.appState
+  };
+})(observer(AccountSend));
