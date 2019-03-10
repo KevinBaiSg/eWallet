ab8594971927fa8540e55dc9b21b303241137e5f
Kevin Bai <kevin.bai.sin@gmail.com>
2019-03-10 18:48:05 +0800

    增加 bitcin summary

--- app/views/Wallet/views/Account/Summary/bitcoin/index.js
+++ app/views/Wallet/views/Account/Summary/bitcoin/index.js
@@ -18,8 +18,6 @@ import AccountBalance from '../components/Balance';
 import AddedToken from '../components/Token';
 import AddTokenMessage from '../components/AddTokenMessage';
 
-import type { Props } from './Container';
-
 const AccountHeading = styled.div`
     padding-bottom: 35px;
     display: flex;
@@ -66,96 +64,96 @@ const AsyncSelectWrapper = styled.div`
 const AddedTokensWrapper = styled.div``;
 
 const AccountSummary = (props: Props) => {
-    const device = props.wallet.selectedDevice;
-    const {
-        account,
-        network,
-        tokens,
-        pending,
-        shouldRender,
-    } = props.selectedAccount;
+  const device = props.wallet.selectedDevice;
+  const {
+    account,
+    network,
+    tokens,
+    pending,
+    shouldRender
+  } = props.selectedAccount;
 
-    if (!device || !account || !network || !shouldRender) {
-        const { loader, exceptionPage } = props.selectedAccount;
-        return <Content loader={loader} exceptionPage={exceptionPage} isLoading />;
-    }
+  if (!device || !account || !network || !shouldRender) {
+    const { loader, exceptionPage } = props.selectedAccount;
+    return <Content loader={loader} exceptionPage={exceptionPage} isLoading/>;
+  }
 
-    const explorerLink: string = `${network.explorer.address}${account.descriptor}`;
-    const pendingAmount: BigNumber = stateUtils.getPendingAmount(pending, network.symbol);
-    const balance: string = new BigNumber(account.balance).minus(pendingAmount).toString(10);
+  const explorerLink: string = `${network.explorer.address}${account.descriptor}`;
+  const pendingAmount: BigNumber = stateUtils.getPendingAmount(pending, network.symbol);
+  const balance: string = new BigNumber(account.balance).minus(pendingAmount).toString(10);
 
-    return (
-        <Content>
-            <React.Fragment>
-                <AccountHeading>
-                    <AccountName>
-                        <CoinLogo network={account.network} />
-                        <AccountTitle>Account #{parseInt(account.index, 10) + 1}</AccountTitle>
-                    </AccountName>
-                    <Link href={explorerLink} isGray>See full transaction history</Link>
-                </AccountHeading>
-                <AccountBalance
-                    network={network}
-                    balance={balance}
-                    fiat={props.fiat}
-                />
-                <H2Wrapper>
-                    <H2>Tokens</H2>
-                    <StyledTooltip
-                        maxWidth={200}
-                        placement="top"
-                        content="Insert token name, symbol or address to be able to send it."
-                    >
-                        <StyledIcon
-                            icon={ICONS.HELP}
-                            color={colors.TEXT_SECONDARY}
-                            size={24}
-                        />
-                    </StyledTooltip>
-                </H2Wrapper>
-                <AsyncSelectWrapper>
-                    <AsyncSelect
-                        isSearchable
-                        defaultOptions
-                        value={null}
-                        isMulti={false}
-                        placeholder="Type in a token name or a token address"
-                        loadingMessage={() => 'Loading...'}
-                        noOptionsMessage={() => 'Token not found'}
-                        onChange={(token) => {
-                            if (token.name) {
-                                const isAdded = tokens.find(t => t.symbol === token.symbol);
-                                if (!isAdded) {
-                                    props.addToken(token, account);
-                                }
-                            }
-                        }}
-                        loadOptions={input => props.loadTokens(input, account.network)}
-                        formatOptionLabel={(option) => {
-                            const isAdded = tokens.find(t => t.symbol === option.symbol);
-                            if (isAdded) {
-                                return `${option.name} (Already added)`;
-                            }
-                            return option.name;
-                        }}
-                        getOptionLabel={option => option.name}
-                        getOptionValue={option => option.symbol}
-                    />
-                </AsyncSelectWrapper>
-                <AddedTokensWrapper>
-                    { tokens.length < 1 && (<AddTokenMessage />) }
-                    {tokens.map(token => (
-                        <AddedToken
-                            key={token.symbol}
-                            token={token}
-                            pending={pending}
-                            removeToken={props.removeToken}
-                        />
-                    ))}
-                </AddedTokensWrapper>
-            </React.Fragment>
-        </Content>
-    );
+  return (
+    <Content>
+      <React.Fragment>
+        <AccountHeading>
+          <AccountName>
+            <CoinLogo network={account.network}/>
+            <AccountTitle>Account #{parseInt(account.index, 10) + 1}</AccountTitle>
+          </AccountName>
+          <Link href={explorerLink} isGray>See full transaction history</Link>
+        </AccountHeading>
+        <AccountBalance
+          network={network}
+          balance={balance}
+          fiat={props.fiat}
+        />
+        <H2Wrapper>
+          <H2>Tokens</H2>
+          <StyledTooltip
+            maxWidth={200}
+            placement="top"
+            content="Insert token name, symbol or address to be able to send it."
+          >
+            <StyledIcon
+              icon={ICONS.HELP}
+              color={colors.TEXT_SECONDARY}
+              size={24}
+            />
+          </StyledTooltip>
+        </H2Wrapper>
+        <AsyncSelectWrapper>
+          <AsyncSelect
+            isSearchable
+            defaultOptions
+            value={null}
+            isMulti={false}
+            placeholder="Type in a token name or a token address"
+            loadingMessage={() => 'Loading...'}
+            noOptionsMessage={() => 'Token not found'}
+            onChange={(token) => {
+              if (token.name) {
+                const isAdded = tokens.find(t => t.symbol === token.symbol);
+                if (!isAdded) {
+                  props.addToken(token, account);
+                }
+              }
+            }}
+            loadOptions={input => props.loadTokens(input, account.network)}
+            formatOptionLabel={(option) => {
+              const isAdded = tokens.find(t => t.symbol === option.symbol);
+              if (isAdded) {
+                return `${option.name} (Already added)`;
+              }
+              return option.name;
+            }}
+            getOptionLabel={option => option.name}
+            getOptionValue={option => option.symbol}
+          />
+        </AsyncSelectWrapper>
+        <AddedTokensWrapper>
+          {tokens.length < 1 && (<AddTokenMessage/>)}
+          {tokens.map(token => (
+            <AddedToken
+              key={token.symbol}
+              token={token}
+              pending={pending}
+              removeToken={props.removeToken}
+            />
+          ))}
+        </AddedTokensWrapper>
+      </React.Fragment>
+    </Content>
+  );
 };
 
 export default AccountSummary;
