8a154ebdb75b7d5e5bde7a628c3bb5235ff2a56a
Kevin Bai <kevin.bai.sin@gmail.com>
2019-03-27 20:34:18 +0800

    实现 EthereumGetAccountInfo

--- app/utils/EthereumGetAccountInfo.js
+++ app/utils/EthereumGetAccountInfo.js
@@ -5,24 +5,25 @@ import { validateParams } from './helpers/paramsValidator';
 import { getEthereumNetwork } from 'utils/data/CoinInfo';
 import Discovery from './helpers/Discovery';
 import { NO_COIN_INFO } from 'constants/errors';
-
 import BlockBook, { create as createBackend } from 'utils/backend';
 import type { CoreMessage, EthereumNetworkInfo } from 'utils/types';
 import type { EthereumAccount } from 'utils/types/account';
-import type { Session as TrezorSession } from 'trezor.js';
-import type { GetAccountInfoOptions } from './GetAccountInfo';
-import Account from './account';
+import type { Session } from 'trezor.js';
+import type { Web3Instance } from 'store/app-state';
+import EthereumjsUnits from 'ethereumjs-units';
 
 type Params = {
   account: EthereumAccount,
   coinInfo: EthereumNetworkInfo,
-  // bundledResponse: boolean,
+  session: Session,
+  web3Instance: Web3Instance
 }
 
 export type EthereumGetAccountInfoOptions = {
   coin?: string,
   account: EthereumAccount,
-  session: TrezorSession,
+  session: Session,
+  web3Instance: Web3Instance
 };
 
 export default class EthereumGetAccountInfo {
@@ -30,23 +31,16 @@ export default class EthereumGetAccountInfo {
   confirmed: boolean = false;
   backend: BlockBook;
   discovery: ?Discovery;
+  web3Instance: any = null;
 
   constructor(options: EthereumGetAccountInfoOptions) {
     this.info = 'Export ethereum account info';
-    this.useDevice = false;
 
-    // const payload: Object = message.payload;
-    // let bundledResponse: boolean = true;
-    // // create a bundle with only one batch
-    // if (!options.hasOwnProperty('accounts')) {
-    //   options.accounts = [...options.account];
-    //   bundledResponse = false;
-    // }
-
-    // validate incoming parameters
     validateParams(options, [
       { name: 'account', type: 'object', obligatory: true },
-      { name: 'coin', type: 'string', obligatory: true }
+      { name: 'coin', type: 'string', obligatory: true },
+      // { name: 'session', type: 'Session', obligatory: true },
+      // { name: 'web3Instance', type: 'Web3Instance', obligatory: true },
     ]);
 
     validateParams(options.account, [
@@ -63,64 +57,50 @@ export default class EthereumGetAccountInfo {
     this.params = {
       account: options.account,
       coinInfo: network,
-      // bundledResponse
+      session: options.session,
+      web3Instance: options.web3Instance,
     };
   }
 
   async run(): Promise<EthereumAccount | Array<EthereumAccount>> {
-    // initialize backend
-    this.backend = await createBackend(this.params.coinInfo);
+    try {
+      this.backend = await createBackend(this.params.coinInfo);
 
-    const blockchain = this.backend.blockchain;
-    const { height } = await blockchain.lookupSyncStatus();
+      const blockchain = this.backend.blockchain;
+      const { height } = await blockchain.lookupSyncStatus();
 
-    const account = this.params.account;
-    const method = 'getAddressHistory';
-    const params = [
-      [account.address],
-      {
-        start: height,
-        end: account.block,
-        from: 0,
-        to: 0,
-        queryMempol: false
-      }
-    ];
-    const socket = await blockchain.socket.promise;
-    const confirmed = await socket.send({ method, params });
-
-    return {
-      address: account.address,
-      transactions: confirmed.totalCount,
-      block: height,
-      balance: '0', // TODO: fetch balance from blockbook
-      availableBalance: '0', // TODO: fetch balance from blockbook
-      nonce: 0 // TODO: fetch nonce from blockbook
-    };
-
-    /*
-    // This will be useful for BTC-like accounts (multi addresses)
-    const addresses: Array<string> = this.params.accounts.map(a => a.address);
-
-    const socket = await blockchain.socket.promise;
-    const method = 'getAddressHistory';
-    const params = [
-        addresses,
+      const account = this.params.account;
+      const method = 'getAddressHistory';
+      const params = [
+        [account.address],
         {
-            start: height,
-            end: 0,
-            from: 0,
-            to: 0,
-            queryMempol: true
+          start: height,
+          end: account.block,
+          from: 0,
+          to: 0,
+          queryMempol: false
         }
-    ];
+      ];
+      const socket = await blockchain.socket.promise;
+      const confirmed = await socket.send({ method, params });
+      // get balance by web3
+      const balance = await this.params.web3Instance.web3.eth.getBalance(account.address);
+      const nonce = await this.params.web3Instance.web3.eth.getTransactionCount(account.address);
 
-    const response = await socket.send({method, params});
-    return {
-        address: addresses[0],
-        transactions: response.totalCount,
-        block: height
+      return {
+        success: true,
+        address: account.address,
+        transactions: confirmed.totalCount,
+        block: height,
+        balance: EthereumjsUnits.convert(balance, 'wei', 'ether'),
+        availableBalance: EthereumjsUnits.convert(balance, 'wei', 'ether'),
+        nonce: nonce,
+      };
+    } catch (e) {
+      return {
+        success: false,
+        error: e,
+      };
     }
-    */
   }
 }
