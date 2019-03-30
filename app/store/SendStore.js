857f98a05c8c618be682f2cb8b84bf7b9a871d7d
Kevin Bai <kevin.bai.sin@gmail.com>
2019-03-30 22:54:02 +0800

    add Send view store and store all state for this view

---
+++ app/store/SendStore.js
@@ -0,0 +1,20 @@
+import { observable, action } from 'mobx';
+import Logger from 'js-logger';
+
+Logger.useDefaults();
+
+let deviceDebug: boolean = false;
+
+if (process.env.NODE_ENV === 'development') {
+  Logger.setLevel(Logger.DEBUG);
+  deviceDebug = true;
+} else if (process.env.DEBUG_PROD === 'true') {
+  Logger.setLevel(Logger.INFO);
+} else {
+  Logger.setLevel(Logger.WARN);
+}
+
+export default class SendStore {
+  @observable
+  test = 'test';
+}
