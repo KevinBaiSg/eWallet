610a6562794baed52f06ff54747b53caec342eab
Kevin Bai <kevin.bai.sin@gmail.com>
2019-04-02 19:24:47 +0800

    set the language is english

--- app/translations/i18n.js
+++ app/translations/i18n.js
@@ -18,7 +18,7 @@ i18n
   .use(reactI18nextModule) // passes i18n down to react-i18next
   .init({
     resources,
-    lng: "zh",
+    lng: "en",
 
     keySeparator: false, // we do not use keys in form messages.welcome
 
