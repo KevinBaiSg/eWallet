239152b653d7f3c5b0a4c06d086bb36cf46344cb
Kevin Bai <kevin.bai.sin@gmail.com>
2019-04-02 15:31:43 +0800

    VerifyAddressTooltip internationalization

--- app/views/Wallet/views/Account/Receive/components/VerifyAddressTooltip/index.js
+++ app/views/Wallet/views/Account/Receive/components/VerifyAddressTooltip/index.js
@@ -1,20 +1,21 @@
 import React from 'react';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { withNamespaces } from "react-i18next";
 
 const Wrapper = styled.div``;
 const Content = styled.div``;
 
-const VerifyAddressTooltip = ({ isConnected, isAvailable, addressUnverified }) => (
+const VerifyAddressTooltip = ({ isConnected, isAvailable, addressUnverified, t }) => (
   <Wrapper>
     {addressUnverified && (
       <Content>
-        Unverified address. {isConnected && isAvailable ? 'Show on Device' : 'Connect your Device to verify it.'}
+        Unverified address. {isConnected && isAvailable ? t('Show on Device') : t('Connect your Device to verify it.')}
       </Content>
     )}
     {!addressUnverified && (
       <Content>
-        {isConnected ? 'Show on Device' : 'Connect your Device to verify address.'}
+        {isConnected ? t('Show on Device') : t('Connect your Device to verify address.')}
       </Content>
     )}
   </Wrapper>
@@ -27,4 +28,4 @@ VerifyAddressTooltip.propTypes = {
 };
 
 
-export default VerifyAddressTooltip;
+export default withNamespaces()(VerifyAddressTooltip);
