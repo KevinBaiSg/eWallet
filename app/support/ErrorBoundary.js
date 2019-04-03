0959f7fd8dda8f1f00394accce20e9c4ce94975f
Kevin Bai <kevin.bai.sin@gmail.com>
2019-04-03 14:53:39 +0800

    format code

--- app/support/ErrorBoundary.js
+++ app/support/ErrorBoundary.js
@@ -3,25 +3,25 @@ import PropTypes from 'prop-types';
 import RedBox from 'redbox-react';
 
 class ErrorBoundary extends PureComponent {
-    constructor(props) {
-        super(props);
-        this.state = { hasError: false, error: false };
-    }
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false, error: false };
+  }
 
-    componentDidCatch(error) {
-        this.setState({ hasError: true, error });
-    }
+  componentDidCatch(error) {
+    this.setState({ hasError: true, error });
+  }
 
-    render() {
-        if (this.state.hasError) {
-            return <RedBox error={this.state.error} />;
-        }
-        return this.props.children;
+  render() {
+    if (this.state.hasError) {
+      return <RedBox error={this.state.error}/>;
     }
+    return this.props.children;
+  }
 }
 
 ErrorBoundary.propTypes = {
-    children: PropTypes.node,
+  children: PropTypes.node
 };
 
 export default ErrorBoundary;
