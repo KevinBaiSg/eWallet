58426c99eacc6b15ea963e8521ae5b41d06e171d
Kevin Bai <kevin.bai.sin@gmail.com>
2019-04-03 17:56:07 +0800

    pass web3Store and web3Actions

--- app/store/index.js
+++ app/store/index.js
@@ -1,27 +1,31 @@
 import AppState from './app-state';
 import SendStore from './SendStore';
+import Web3Store from './Web3Store';
 
-import SendActions from './SendActions'
+import SendActions from './SendActions';
+import Web3Actions from './Web3Actions';
 
-export {
-  AppState,
-  SendStore
-};
-
-export default {
-  AppState,
-  SendStore
-};
+// export {
+//   AppState,
+//   SendStore
+// };
+//
+// export default {
+//   AppState,
+//   SendStore
+// };
 
 export const CreateStoreMap = () => {
   return {
     appState: new AppState(),
     sendStore: new SendStore(),
+    web3Store: new Web3Store(),
   };
 };
 
 export const CreateActionMap = (stores) => {
   return {
     sendActions: new SendActions(stores),
+    web3Actions: new Web3Actions(stores),
   }
 };
