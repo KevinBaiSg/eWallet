4760b4d84f1384831dd73a9409c735650baace0a
Kevin Bai <kevin.bai.sin@gmail.com>
2019-04-05 19:50:51 +0800

    remove the device from pin invalid

--- app/components/modals/pin/Invalid/index.js
+++ app/components/modals/pin/Invalid/index.js
@@ -1,31 +1,23 @@
 /* @flow */
 
 import React from 'react';
-import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
 import { H3 } from 'components/Heading';
 import P from 'components/Paragraph';
 
-import type { TrezorDevice } from 'flowtype';
-
-type Props = {
-  device: TrezorDevice;
-}
-
 const Wrapper = styled.div`
     padding: 30px 48px;
 `;
 
-const InvalidPin = (props: Props) => (
+const InvalidPin = () => (
   <Wrapper>
-    <H3>Entered PIN for {props.device.label} is not correct</H3>
+    <H3>Entered PIN for is not correct</H3>
     <P isSmaller>Retrying...</P>
   </Wrapper>
 );
 
 InvalidPin.propTypes = {
-  device: PropTypes.object.isRequired
 };
 
 export default InvalidPin;
