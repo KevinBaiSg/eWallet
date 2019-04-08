28a769b1a09f18c642a961804511c68ed7a14231
Kevin Bai <kevin.bai.sin@gmail.com>
2019-04-08 19:00:57 +0800

    pass InitializeStore and InitializeActions to stores

--- app/store/index.js
+++ app/store/index.js
@@ -2,10 +2,12 @@ import AppState from './app-state';
 import SendStore from './SendStore';
 import Web3Store from './Web3Store';
 import PinStore from './PinStore';
+import InitializeStore from './InitializeStore';
 
 import SendActions from './SendActions';
 import Web3Actions from './Web3Actions';
 import PinActions from './PinActions'
+import InitializeActions from './InitializeActions'
 
 export const CreateStoreMap = (history) => {
   return {
@@ -13,6 +15,7 @@ export const CreateStoreMap = (history) => {
     sendStore: new SendStore(),
     web3Store: new Web3Store(),
     pinStore: new PinStore(),
+    initializeStore: new InitializeStore(),
   };
 };
 
@@ -21,5 +24,6 @@ export const CreateActionMap = (stores) => {
     sendActions: new SendActions(stores),
     web3Actions: new Web3Actions(stores),
     pinActions: new PinActions(stores),
+    initializeActions: new InitializeActions(stores),
   }
 };
